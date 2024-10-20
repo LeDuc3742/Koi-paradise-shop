fbc230548feef19dff8e1d1569fb00fc4dcbd70f
LeDuc3742 <leminhduc2623@gmail.com>
2024-10-20 21:38:02 +0700

    Delete backend directory

--- backend/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from models import Product, db
-
-main_routes = Blueprint('main', __name__)
-
-@main_routes.route('/api/products', methods=['GET'])
-def get_products():
-    products = Product.query.all()
-    return jsonify([{'id': p.id, 'name': p.name, 'price': p.price, 'image': p.image} for p in products])
